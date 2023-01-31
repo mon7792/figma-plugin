e12bae794226a7249de12df0a5bd5c110dc094fd
mon7792 <monish7792@gmail.com>
2023-01-31 23:39:05 +0000

    added placeholder components

--- wireframe-kit/src/components/Placeholder/Placeholder.jsx
+++ wireframe-kit/src/components/Placeholder/Placeholder.jsx
@@ -1,23 +1,68 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import '../../style/style.css';
+import React from "react";
+import PropTypes from "prop-types";
+import "../../style/style.css";
 
-import Picture from "../../assets/icon/Picture.svg"
+import Picture from "../../assets/icon/Picture.svg";
+import User from "../../assets/icon/User.svg";
+import File from "../../assets/icon/File.svg";
+import Folder from "../../assets/icon/Folder.svg";
+import Map from "../../assets/icon/Map.svg";
+import Play from "../../assets/icon/Play.svg";
+import Tag from "../../assets/icon/Tag.svg";
+import Music from "../../assets/icon/Music.svg";
+import Process from "../../assets/icon/Process.svg";
 
 /**
  * Primary UI component for user interaction
  */
-export const Placeholder = ({}) => {
-  let className = `sr`
-  return (
-    <div className={className}>
-        <img src={Picture} />
-    </div>
-  );
+export const Placeholder = ({ instance }) => {
+  let imgSrc;
+  switch (instance) {
+    case "Picture":
+      imgSrc = Picture;
+      break;
+    case "User":
+      imgSrc = User;
+      break;
+    case "File":
+      imgSrc = File;
+      break;
+    case "Folder":
+      imgSrc = Folder;
+      break;
+    case "Map":
+      imgSrc = Map;
+      break;
+    case "Play":
+      imgSrc = Play;
+      break;
+    case "Tag":
+      imgSrc = Tag;
+      break;
+    case "Music":
+      imgSrc = Music;
+      break;
+    default:
+      imgSrc = Process;
+      break;
+  }
+  return <img src={imgSrc} />;
 };
 
+Placeholder.propTypes = {
+  instance: PropTypes.oneOf([
+    "Picture",
+    "User",
+    "File",
+    "Folder",
+    "Map",
+    "Play",
+    "Tag",
+    "Music",
+    "Process",
+  ]),
+};
 
-Placeholder.propTypes = {};
-  
-Placeholder.defaultProps = {};
-  
+Placeholder.defaultProps = {
+  instance: "Picture",
+};
--- wireframe-kit/src/stories/Placeholder.stories.jsx
+++ wireframe-kit/src/stories/Placeholder.stories.jsx
@@ -1,13 +1,72 @@
-import React from 'react';
+import React from "react";
 
-import { Placeholder } from '../components/Placeholder/Placeholder';
+import { Placeholder } from "../components/Placeholder/Placeholder";
 
 export default {
-  title: 'Primitive/Placeholder',
-  component: Placeholder
-
+  title: "Primitive/Placeholder",
+  component: Placeholder,
+  argTypes: {
+    instance: {
+      control: "select",
+      options: [
+        "Picture",
+        "User",
+        "File",
+        "Folder",
+        "Map",
+        "Play",
+        "Tag",
+        "Music",
+        "Process",
+      ],
+      defaultValue: "Picture",
+    },
+  },
 };
 
 const Template = (args) => <Placeholder {...args} />;
 
 export const Picture = Template.bind({});
+Picture.args = {
+  instance: "Picture",
+};
+
+export const User = Template.bind({});
+User.args = {
+  instance: "User",
+};
+
+export const File = Template.bind({});
+File.args = {
+  instance: "File",
+};
+
+export const Folder = Template.bind({});
+Folder.args = {
+  instance: "Folder",
+};
+
+export const Map = Template.bind({});
+Map.args = {
+  instance: "Map",
+};
+
+export const Play = Template.bind({});
+Play.args = {
+  instance: "Play",
+};
+
+export const Tag = Template.bind({});
+Tag.args = {
+  instance: "Tag",
+};
+
+export const Music = Template.bind({});
+Music.args = {
+  instance: "Music",
+};
+
+export const Process = Template.bind({});
+Process.args = {
+  instance: "Process",
+};
