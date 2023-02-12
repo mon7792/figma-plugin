7a3a4cb80570633b0e234e05ed5dccc79b28756b
mon7792 <monish7792@gmail.com>
2023-02-12 13:15:20 +0000

    added comments

--- pix2figma/src/code.ts
+++ pix2figma/src/code.ts
@@ -17,7 +17,7 @@ figma.ui.onmessage = msg => {
 
     // ADD CODE HERE.
     const screen = readDSL()
-    // 
+    
     figma.currentPage.appendChild(screen);
     nodes.push(screen);
 
@@ -30,87 +30,3 @@ figma.ui.onmessage = msg => {
   // keep running, which shows the cancel button at the bottom of the screen.
   figma.closePlugin();
 };
-
-
-
-// header {
-//   btn-inactive, btn-inactive, btn-inactive, btn-active, btn-inactive
-//   }
-//   row {
-//   single {
-//   small-title, text, btn-red
-//   }
-//   }
-
-
-// function screenAndButton(){
-//       // 
-//       const startBtnComponent = figma.root.findOne(
-//         (node) => node.type === "COMPONENT_SET" && node.name == "Button"
-//       ) as ComponentSetNode;
-  
-//       console.log(startBtnComponent.children[0].name)
-//       const starButton = startBtnComponent.findOne((node) => node.name === "Type=Circle Icon, Fill=False, Dark=True") as ComponentNode
-  
-//       const strBtn = starButton.createInstance()
-//       // const defaultVariant = startBtnComponent.defaultVariant as ComponentNode;
-  
-  
-//       // add the blank frame
-//       const blankScreenFrame = figma.root.findOne(
-//         (node) => node.type === "FRAME" && node.name == "Blank Screen"
-//       ) as FrameNode;
-  
-//       blankScreenFrame.appendChild(strBtn)
-//       blankScreenFrame.clone();
-  
-// }
-  
-
-    // for (let i = 0; i < 5; i++) {
-
-    //   const rect = figma.createRectangle();
-    //   rect.x = i * 150;
-    //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-    //   figma.currentPage.appendChild(rect);
-    //   nodes.push(rect);
-    // }
-
-
-// function header(){
-//   const startBtnComponent = figma.root.findOne(
-//     (node) => node.type === "COMPONENT_SET" && node.name == "Button"
-//   ) as ComponentSetNode;
-
-//   const starButton = startBtnComponent.findOne((node) => node.name === "Type=Circle Icon, Fill=False, Dark=True") as ComponentNode
-//   // const strBtn = starButton.createInstance()
-
-//   //  create a frame node
-//   const rowFrame = figma.createFrame();
-//   rowFrame.name = "rwo-single"
-
-//   // structure: resize to entire desktop width.
-//   rowFrame.resize(1280, 832)
-
-//   // add the layout
-//   rowFrame.layoutPositioning = "AUTO"
-//   rowFrame.layoutMode = "VERTICAL"
-
-//   // Space between children
-//   rowFrame.horizontalPadding = 10
-//   rowFrame.verticalPadding = 10
-
-//   rowFrame.itemSpacing = 50
-
-//   rowFrame.fills = [{type: 'SOLID', color: {r: 0, g: 0.49, b: 0.77}}];
-//   // 00314D
-
-
-//   // children
-//   rowFrame.appendChild(starButton.createInstance())
-//   rowFrame.appendChild(starButton.createInstance())
-
-
-//   // figma.currentPage.appendChild(rowFrame);
-//   // nodes.push(rowFrame);
-// }
--- pix2figma/src/pix/pix.ts
+++ pix2figma/src/pix/pix.ts
@@ -4,7 +4,7 @@ type pixFigmaNodeType =
   | "pix2fig-text"
   | "pix2fig-small-title";
 
-type pixFigmaDSLType = "pix2fig-row" | "pix2fig-single";
+type pixFigmaDSLType = "pix2fig-header" | "pix2fig-row" | "pix2fig-single";
 
 // pixComponentPage returns the page containing pix figma component.
 const pixComponentPage = figma.root.findOne(
@@ -31,7 +31,7 @@ function createTitle(componentPage: PageNode): InstanceNode {
   return title.clone();
 }
 
-// createTitle return the instance of Title.
+// createText return the instance of Text.
 function createText(componentPage: PageNode): InstanceNode {
   const title = componentPage.findOne(
     (node) => node.type === "INSTANCE" && node.name === "pix2fig-text"
@@ -41,35 +41,7 @@ function createText(componentPage: PageNode): InstanceNode {
   return title.clone();
 }
 
-// TODO: createHorizontalContainer , create Vertical COnta
-// createContainer
-function createContainer(instances: Array<InstanceNode>): FrameNode {
-  const rowFrame = figma.createFrame();
-  rowFrame.name = "rwo-single";
-
-  // structure: resize to entire desktop width.
-  rowFrame.resize(1280, 100);
-
-  // add the layout
-  rowFrame.layoutPositioning = "AUTO";
-  rowFrame.layoutMode = "HORIZONTAL";
-  rowFrame.primaryAxisSizingMode = "FIXED";
-  rowFrame.counterAxisSizingMode = "AUTO";
-
-  // Space between children
-  rowFrame.horizontalPadding = 10;
-  rowFrame.verticalPadding = 10;
-  rowFrame.itemSpacing = 10;
-
-  rowFrame.fills = [{ type: "SOLID", color: { r: 0, g: 0.14, b: 0.23 } }];
-  // 00314D
-
-  // children
-  instances.map((instance) => rowFrame.appendChild(instance));
-  return rowFrame;
-}
-
-// createScreen returns the instance of blank screen.
+// createScreen returns the instance of blank desktop screen.
 function createScreen(
   nodeType: pixFigmaNodeType,
   componentPage: PageNode
@@ -94,12 +66,34 @@ function createScreen(
   return blankScreenFrame;
 }
 
-// pixHeader returns the figmaPixHeader
+// pixHeader returns a frame node with full width by adding instances horizontally
 function pixHeader(instances: Array<InstanceNode>): FrameNode {
-  return createContainer(instances);
+  const rowFrame = figma.createFrame();
+  rowFrame.name = "pix2fig-header";
+
+  // structure: resize to entire desktop width.
+  rowFrame.resize(1280, 100);
+
+  // add the layout
+  rowFrame.layoutPositioning = "AUTO";
+  rowFrame.layoutMode = "HORIZONTAL";
+  rowFrame.primaryAxisSizingMode = "FIXED";
+  rowFrame.counterAxisSizingMode = "AUTO";
+
+  // Space between children
+  rowFrame.horizontalPadding = 10;
+  rowFrame.verticalPadding = 10;
+  rowFrame.itemSpacing = 10;
+
+  rowFrame.fills = [{ type: "SOLID", color: { r: 0, g: 0.14, b: 0.23 } }];
+  // 00314D
+
+  // children
+  instances.map((instance) => rowFrame.appendChild(instance));
+  return rowFrame;
 }
 
-// createRowContainer c
+// pixRow creates a frame node with full width by adding instances horizontally
 function pixRow(instances: Array<FrameNode>): FrameNode {
   const rowFrame = figma.createFrame();
   rowFrame.name = "pix2fig-row";
@@ -126,27 +120,25 @@ function pixRow(instances: Array<FrameNode>): FrameNode {
   return rowFrame;
 }
 
-// createRowContainer c
+// pixSingle creates a frame node with full width by adding instances vertically
 function pixSingle(instances: Array<InstanceNode>): FrameNode {
+  // create frame
   const rowFrame = figma.createFrame();
   rowFrame.name = "pix2fig-single";
 
-  // structure: resize to entire desktop width.
+  // add the layout
+  // resize to desktop width.
   rowFrame.resize(1280, 100);
 
-  // add the layout
   rowFrame.layoutPositioning = "AUTO";
   rowFrame.layoutMode = "VERTICAL";
-  // rowFrame.primaryAxisSizingMode = "FIXED";
-  // rowFrame.counterAxisSizingMode = "AUTO";
 
-  // Space between children
   rowFrame.horizontalPadding = 10;
   rowFrame.verticalPadding = 10;
   rowFrame.itemSpacing = 10;
 
+  // fills 00314D
   rowFrame.fills = [{ type: "SOLID", color: { r: 0, g: 0.14, b: 0.23 } }];
-  // 00314D
 
   // children
   instances.map((instance) => rowFrame.appendChild(instance));
@@ -168,9 +160,40 @@ export function readDSL(): SceneNode {
   const single = pixSingle([title, text, button]);
   const row = pixRow([single]);
 
-  
   screen.appendChild(header);
   screen.appendChild(row);
 
   return screen;
 }
+
+/*
+SAMPLE:
+header {
+btn-inactive, btn-inactive, btn-inactive, btn-active, btn-inactive
+}
+row {
+single {
+small-title, text, btn-red
+}
+}
+
+RESULT:
+const screen = createScreen("pix2fig-screen", pixComponentPage);
+  const header = pixHeader([
+    createButton(pixComponentPage),
+    createButton(pixComponentPage),
+  ]);
+
+  const title = createTitle(pixComponentPage);
+  const text = createText(pixComponentPage);
+  const button = createButton(pixComponentPage);
+
+  const single = pixSingle([title, text, button]);
+  const row = pixRow([single]);
+
+  
+  screen.appendChild(header);
+  screen.appendChild(row);
+
+  return screen;
+*/
