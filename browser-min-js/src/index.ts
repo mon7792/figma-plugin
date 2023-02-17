9f3952315d941f3556b2d3ea2d0489c3b80e3eae
mon7792 <monish7792@gmail.com>
2023-02-17 21:01:05 +0000

    added pixNodestring reader

--- browser-min-js/src/index.ts
+++ browser-min-js/src/index.ts
@@ -4,54 +4,60 @@ import { HNode, NodeType } from "./html/html";
 import {showCssNode} from "./css/css";
 
 
-// showNode recursive version
-function showNode(nd: HNode) {
-  if (nd === null) {
-    return;
-  }
-  if (nd.type === NodeType.TEXT) {
-    if (typeof nd.data === "string") {
-      console.log(nd.data);
-    }
-  } else {
-    if (typeof nd.data !== "string") {
-      console.log(nd.data.tagName);
-    }
-  }
-  if (nd.children == null || nd.children.length === 0) {
-    return;
-  }
+import { readDSL } from "./pix/pix";
 
-  for (let i = 0; i < nd.children.length; i++) {
-    showNode(nd.children[i]);
-  }
-}
+
+// showNode recursive version
+// function showNode(nd: HNode) {
+//   if (nd === null) {
+//     return;
+//   }
+//   if (nd.type === NodeType.TEXT) {
+//     if (typeof nd.data === "string") {
+//       console.log(nd.data);
+//     }
+//   } else {
+//     if (typeof nd.data !== "string") {
+//       console.log(nd.data.tagName);
+//     }
+//   }
+//   if (nd.children == null || nd.children.length === 0) {
+//     return;
+//   }
+
+//   for (let i = 0; i < nd.children.length; i++) {
+//     showNode(nd.children[i]);
+//   }
+// }
 
 // sampleHTML docs
-let sampleHTML: string = `<html><head><title>sample</title></head><body><h1>H1</h1><div><button>SUBMIT</button></div></body></html>`;
+// let sampleHTML: string = `<html><head><title>sample</title></head><body><h1>H1</h1><div><button>SUBMIT</button></div></body></html>`;
 
 
-const pars = new HTMLParser(sampleHTML)
-console.log("PARSING HTML....")
-let t = pars.parse()
-console.log(t)
-showNode(t)
+// const pars = new HTMLParser(sampleHTML)
+// console.log("PARSING HTML....")
+// let t = pars.parse()
+// console.log(t)
+// showNode(t)
 
 
-console.log("PARSING CSS....")
-console.log("PARSING CSS....")
+// console.log("PARSING CSS....")
+// console.log("PARSING CSS....")
 
-let sampleCss = `
-h1 { margin: auto; color: #cc0000; }
-div.note { margin-bottom: 20px; padding: 10px; }
-#answer { display: none; }
-`
-
-// h1, h2, h3 { margin: auto; color: #cc0000; }
+// let sampleCss = `
+// h1 { margin: auto; color: #cc0000; }
 // div.note { margin-bottom: 20px; padding: 10px; }
 // #answer { display: none; }
+// `
 
-const cssPar = new CssParser(sampleCss);
-let ct = cssPar.parse()
-showCssNode(ct)
+// // h1, h2, h3 { margin: auto; color: #cc0000; }
+// // div.note { margin-bottom: 20px; padding: 10px; }
+// // #answer { display: none; }
 
+// const cssPar = new CssParser(sampleCss);
+// let ct = cssPar.parse()
+// showCssNode(ct)
+
+
+console.log("READ DSL......")
+readDSL();
---
+++ browser-min-js/src/pix/pix.ts
@@ -0,0 +1,161 @@
+const pixNodeList = [
+  "body",
+  "header",
+  "btn-active",
+  "btn-inactive",
+  "row",
+  "single",
+  "double",
+  "quadruple",
+  "btn-green",
+  "btn-orange",
+  "btn-red",
+  "big-title",
+  "small-title",
+  "def",
+] as const;
+
+type pixNodeType = typeof pixNodeList[number];
+
+//  check if the node type is valid
+// function isNodeType(nd: string):nd is pixNodeType{
+//     let check = false
+//     pixNodeList.forEach(pxNode => {
+//         if (pxNode === nd){
+//             check = true
+//             break
+//         }
+//     })
+//     return check
+// }
+
+// getPixNodeType typecast nd string into pixNodeType.
+function getPixNodeType(nd: string): pixNodeType | undefined {
+  return pixNodeList.filter((pixNd) => pixNd === nd)[0];
+}
+
+// pixNode represent the
+export type pixNode = {
+  name: pixNodeType;
+  children: Array<pixNode>;
+};
+
+const dslInput = `
+header {
+    btn-inactive, btn-inactive, btn-inactive, btn-active, btn-inactive
+    }
+    row {
+    single {
+    small-title, text, btn-red
+    }
+    }
+`;
+
+// getPixNode return the pixNodeTree from input.
+function getPixNode(input: string): pixNode {
+  // 1. trim white spaces at the start and end.
+  input = input.trim();
+
+  //2. variable declaration
+  // token store pixNodeType
+  let token = "";
+  // pxNode store entire node tree
+  let pxNode: pixNode = { name: "body", children: [] };
+  // pixTokenStack
+  let pixTokenStack: Array<pixNode> = [];
+  pixTokenStack.push(pxNode);
+
+  for (let i = 0; i < input.length; i++) {
+    // { element
+    if (input[i] === "{") {
+      token = token.trim();
+      // TODO: check if the node is valid
+      // get node type from token
+      let pTkn = getPixNodeType(token);
+      if (pTkn === undefined) {
+        // TODO: deal with this
+        continue;
+      }
+      // create a new new node
+      let pixToken: pixNode = { name: pTkn, children: [] };
+
+      if (pixTokenStack.length === 0) {
+        pixTokenStack.push(pixToken);
+      } else {
+        // add the element to children
+        pixTokenStack[pixTokenStack.length - 1].children.push(pixToken);
+        //  push it to top of stack
+        pixTokenStack.push(pixToken);
+      }
+
+      // reset the token
+      token = "";
+      continue;
+    }
+
+    // , element
+    if (input[i] === ",") {
+      token = token.trim();
+      // TODO: check if the node is valid
+      // get node type from token
+      let pTkn = getPixNodeType(token);
+      if (pTkn === undefined) {
+        // TODO: deal with this
+        continue;
+      }
+      // create a new new node
+      let pixToken: pixNode = { name: pTkn, children: [] };
+
+      // push the token in the current element stack
+      pixTokenStack[pixTokenStack.length - 1].children.push(pixToken);
+
+      // reset the token
+      token = "";
+      continue;
+    }
+
+    // } element
+    if (input[i] === "}") {
+      token = token.trim();
+      // TODO: check if the node is valid
+      // get node type from token
+      let pTkn = getPixNodeType(token);
+      if (pTkn === undefined) {
+        // TODO: deal with this
+        continue;
+      }
+      // create a new new node
+      let pixToken: pixNode = { name: pTkn, children: [] };
+
+      // add the element to children
+      pixTokenStack[pixTokenStack.length - 1].children.push(pixToken);
+      //  pop it to top of stack
+      pixTokenStack.pop();
+
+      // reset the token
+      token = "";
+      continue;
+    }
+    token = token + input[i];
+  }
+
+  return pxNode;
+}
+
+export function readDSL() {
+  console.log("reading DSL");
+  let nd = getPixNode(dslInput);
+  console.log();
+  displayPixNode(nd);
+}
+
+function displayPixNode(pxNode: pixNode) {
+  if (pxNode === undefined) {
+    return;
+  }
+  console.log(pxNode.name);
+
+  for (let i = 0; i < pxNode.children.length; i++) {
+    displayPixNode(pxNode.children[i]);
+  }
+}
