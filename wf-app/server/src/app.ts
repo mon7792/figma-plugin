c54b50e546a9faad0c0c1f4976c45747b24bac93
mon7792 <monish7792@gmail.com>
2023-06-04 20:09:56 +0530

    initial api design

---
+++ wf-app/server/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Express } from "express";
+import cors from "cors";
+import todoRoutes from "./routes";
+
+const app: Express = express();
+
+const PORT: string | number = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(todoRoutes);
+
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
---
+++ wf-app/server/src/types/todos.ts
@@ -0,0 +1,6 @@
+// ITodo holds information about todo   
+export interface ITodo {
+    name: string
+    description: string
+    status: boolean
+  }
