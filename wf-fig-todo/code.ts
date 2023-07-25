071f56316222ac6fe23b7a08b3cd2c1f60bef28a
mon7792 <monish7792@gmail.com>
2023-07-25 21:11:19 +0100

    added drag and drop

--- wf-fig-todo/code.ts
+++ wf-fig-todo/code.ts
@@ -37,9 +37,54 @@ if (figma.editorType === "figma") {
       figma.viewport.scrollAndZoomIntoView(nodes);
     }
 
+    if (msg.type === "drop-img") {
+      console.log("drop image");
+      const { dropPosition, windowSize, offset, itemSize, src } = msg;
+
+      // Getting the position and size of the visible area of the canvas.
+      const bounds = figma.viewport.bounds;
+
+      // Getting the zoom level
+      const zoom = figma.viewport.zoom;
+
+      // There are two states of the Figma interface: With or without the UI (toolbar + left and right panes)
+      // The calculations would be slightly different, depending on whether the UI is shown.
+      // So to determine if the UI is shown, we'll be comparing the bounds to the window's width.
+      // Math.round is used here because sometimes bounds.width * zoom may return a floating point number very close but not exactly the window width.
+      const hasUI = Math.round(bounds.width * zoom) !== windowSize.width;
+
+      // Since the left pane is resizable, we need to get its width by subtracting the right pane and the canvas width from the window width.
+      const leftPaneWidth = windowSize.width - bounds.width * zoom - 240;
+
+      // Getting the position of the cursor relative to the top-left corner of the canvas.
+      const xFromCanvas = hasUI
+        ? dropPosition.clientX - leftPaneWidth
+        : dropPosition.clientX;
+      const yFromCanvas = hasUI
+        ? dropPosition.clientY - 40
+        : dropPosition.clientY;
+
+      // Create a rectangle
+      const rect = await createImage(src);
+
+      // Resize the rectangle to be the same size as the item from the plugin window.
+      // rect.resize(itemSize.width, itemSize.height);
+      figma.currentPage.appendChild(rect);
+
+      // The canvas position of the drop point can be calculated using the following:
+      rect.x = bounds.x + xFromCanvas / zoom - offset.x;
+      rect.y = bounds.y + yFromCanvas / zoom - offset.y;
+
+      // Select the rectangle
+      figma.currentPage.selection = [rect];
+    }
+
     // Make sure to close the plugin when you're done. Otherwise the plugin will
     // keep running, which shows the cancel button at the bottom of the screen.
-    figma.closePlugin();
+
+    if (msg.type === "cancel") {
+      figma.closePlugin();
+    }
   };
 }
 
@@ -130,3 +175,23 @@ async function name(charCount: number) {
 
 //   figma.closePlugin();
 // })();
+
+
+async function createImage(src: string): Promise<RectangleNode> {
+  const image = await figma.createImageAsync(src);
+
+  const node = figma.createRectangle()
+
+  const { width, height } = await image.getSizeAsync()
+  node.resize(width, height)
+
+  // Render the image by filling the rectangle.
+  node.fills = [
+    {
+      type: 'IMAGE',
+      imageHash: image.hash,
+      scaleMode: 'FILL'
+    }
+  ]
+  return node;
+}
