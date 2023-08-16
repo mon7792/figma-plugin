670c36e331e316415a411a847b96c0289a0ac189
mon7792 <monish7792@gmail.com>
2023-08-16 20:30:44 +0100

    basic mock structure

---
+++ docs/svg-gen.md
@@ -0,0 +1,14 @@
+# SVG GEN
+The `SVG GEN` is the plugin to generate svg.
+
+## Problem:
+The SVG Gen gives user the ability to generate SVG within the figma itself.
+
+
+
+## User Flow.
+1. User should be able to login and see the credits.
+2. User should be able to generate svg using the text.
+3. user should be able to drag and drop the svg within figma.  
+
+
---
+++ wf-app/mock/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+
+
+let svgResp = [{
+    id: "1234",
+    name: "Coffee Time",
+    url: "http://127.0.0.1:8080/svg/coffee.svg"
+},
+{
+    id: "5678",
+    name: "Expecting it",
+    url: "http://127.0.0.1:8080/svg/expecting.svg"
+},
+{
+    id: "9012",
+    name: "Love It",
+    url: "http://127.0.0.1:8080/svg/love.svg"
+},
+{
+    id: "3456",
+    name: "Walking",
+    url: "http://127.0.0.1:8080/svg/walk.svg"
+}]
+
+app.use('/svg', express.static('svg'))
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// generate get content in the body and return list of svg images
+app.post('/generate', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    res.send(JSON.stringify(svgResp))
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
